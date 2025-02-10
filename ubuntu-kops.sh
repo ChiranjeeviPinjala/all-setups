cd38ce805694d62a1e51756ec482a5cf19ca9d35
ChiranjeeviPinjala <97869807+ChiranjeeviPinjala@users.noreply.github.com>
2025-02-10 14:06:44 +0530

    Update ubuntu-kops.sh

--- ubuntu-kops.sh
+++ ubuntu-kops.sh
@@ -1,24 +1,18 @@
 #vim .bashrc
 #export PATH=$PATH:/usr/local/bin/
 #source .bashrc
-
-
-#! /bin/bash
-apt update -y
-apt upgrade -y
-apt install awscli -y
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install
-# aws configure
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
+chmod +x kubectl
 chmod +x kops-linux-amd64 kubectl
-mv kubectl /usr/local/bin/kubectl
+mv kubectl /usr/local/bin/
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket cloudanddevopsbyraham007899123.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham007899123.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham007899123.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
-kops update cluster --name rahams.k8s.local --yes --admin
+aws s3 mb s3://unqbucketname.devops
+aws s3api put-bucket-versioning --bucket unqbucketname.devops --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://unqbucketname.devops
+kops create cluster --name clustername.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --master-volume-size 27 --node-count=2 --node-volume-size 25 --node-size t2.micro
+kops update cluster --name clustername.k8s.local --yes --admin
