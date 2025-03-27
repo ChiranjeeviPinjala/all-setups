f7d633e7fddc871aa2bd20d074a5111b7178c18f
chiranjeevi <97869807+ChiranjeeviPinjala@users.noreply.github.com>
2025-03-27 16:46:41 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,2 +1,8 @@
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
+
+# To install Vault
+
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install vault
